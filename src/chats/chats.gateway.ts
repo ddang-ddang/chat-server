c94964e902e5fcb1eae1a1bc34b358e4de990d0c
moto4321 <moto432195@gmail.com>
2022-05-07 18:54:06 +0900

    [refactor] 채팅 기능 분리

---
+++ src/chats/chats.gateway.ts
@@ -0,0 +1,51 @@
+import { Logger } from '@nestjs/common';
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+} from '@nestjs/websockets';
+import { Server } from 'http';
+import { Socket } from 'socket.io';
+import { ChatsService } from './chats.service';
+import { CreateChatDto } from './dto/create-chat.dto';
+import { UpdateChatDto } from './dto/update-chat.dto';
+
+@WebSocketGateway()
+export class ChatsGateway {
+  server: Server;
+
+  // private logger = new Logger('')
+  constructor(private readonly chatsService: ChatsService) {}
+
+  @SubscribeMessage('creaetRoom')
+  async createRoom(socket: Socket, data: string) {
+    socket.join('aRoom');
+    socket.to('aRoom').emit('roomCreated', { room: 'aRoom' });
+    return { event: 'roomCreated', room: 'aRoom' };
+  }
+
+  @SubscribeMessage('createChat')
+  async createChat(@MessageBody() createChatDto: CreateChatDto) {
+    const message = await this.chatsService.createChat(createChatDto);
+    this.server.emit('message', message);
+    return message;
+  }
+
+  @SubscribeMessage('findAllChats')
+  findAll() {
+    return this.chatsService.findAll();
+  }
+
+  @SubscribeMessage('removeChat')
+  remove(@MessageBody() id: number) {
+    return this.chatsService.remove(id);
+  }
+
+  handleConnection(client: Socket) {
+    // this.logger.log(`Client connected`);
+  }
+
+  handleDisconnction(client: Socket) {
+    // this.logger.log(`Client disconnected`);
+  }
+}
